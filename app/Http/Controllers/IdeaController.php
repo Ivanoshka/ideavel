842e61cd8d4bdedf1ebfe400fc0c87263cc5c208
Ivanoshka <19030188@itcelaya.edu.mx>
2024-07-10 14:12:59 -0600

    listamos los registros de ideas, de la base de datos

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class IdeaController extends Controller
 {
     public function index(){
-        return view('ideas.index');
+
+        //nos conectamos a la base de datos, a la tabla ideas
+
+        //select * from ideas
+        $ideas = DB::table('ideas')->get();
+
+
+        return view('ideas.index',['ideas'=> $ideas]); //nos retorna la vista, y manda las ideas en la vista como varibles 'ideas'
     }
 }
